06d42129e248c92c1760d21540513e3431a70235
sean sulham <sean.sulham@hotmail.com>
2019-01-05 16:08:42 -0500

    CartDAOImpl and PurchasedDAOImpl

--- HighTechIndustries/src/main/java/project2/hightechindustries/dao/CartDAOImpl.java
+++ HighTechIndustries/src/main/java/project2/hightechindustries/dao/CartDAOImpl.java
@@ -1,39 +1,69 @@
 package project2.hightechindustries.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import project2.hightechindustries.beans.Cart;
+import project2.hightechindustries.util.HibernateUtil;
 
 public class CartDAOImpl implements CartDAO {
+	
+	private SessionFactory sf = HibernateUtil.getSessionFactory();
 
 	@Override
 	public Cart getCartById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Cart item = null;
+		try (Session s = sf.getCurrentSession()){
+			Transaction tx = s.beginTransaction();
+			item = (Cart) s.get(Cart.class, id);
+			tx.commit();
+			s.close();
+		}
+		return item;
 	}
 
 	@Override
 	public List<Cart> getAllCartItems() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Cart> items = new ArrayList<>();
+		try (Session s = sf.getCurrentSession()){
+			Transaction tx = s.beginTransaction();
+			items = s.createQuery("from Cart").getResultList();
+			tx.commit();
+			s.close();
+		}
+		return items;
 	}
 
 	@Override
 	public void addCartItem(Cart c) {
-		// TODO Auto-generated method stub
-		
+		try (Session s = sf.getCurrentSession()){
+			Transaction tx = s.beginTransaction();
+			s.persist(c);
+			tx.commit();
+			s.close();
+		}
 	}
 
 	@Override
 	public void updateCart(Cart c) {
-		// TODO Auto-generated method stub
-		
+		try (Session s = sf.getCurrentSession()){
+			Transaction tx = s.beginTransaction();
+			s.update(c);
+			s.close();
+		}
 	}
 
 	@Override
 	public void deleteItem(Cart c) {
-		// TODO Auto-generated method stub
-		
+		try (Session s = sf.getCurrentSession()){
+			Transaction tx = s.beginTransaction();
+			s.delete(c);
+			s.close();
+		}
 	}
 
 }
--- HighTechIndustries/src/main/java/project2/hightechindustries/dao/PurchasedDAOImpl.java
+++ HighTechIndustries/src/main/java/project2/hightechindustries/dao/PurchasedDAOImpl.java
@@ -1,39 +1,71 @@
 package project2.hightechindustries.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import project2.hightechindustries.beans.Purchased;
+import project2.hightechindustries.util.HibernateUtil;
 
 public class PurchasedDAOImpl implements PurchasedDAO {
 
+	private SessionFactory sf = HibernateUtil.getSessionFactory();
+	
 	@Override
 	public Purchased getPurchasedItemById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Purchased item = null;
+		try (Session s = sf.getCurrentSession()){
+			Transaction tx = s.beginTransaction();
+			item = (Purchased) s.get(Purchased.class, id);
+			tx.commit();
+			s.close();
+		}
+		return item;
 	}
 
 	@Override
 	public List<Purchased> getAllPurchased() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Purchased> items = new ArrayList<>();
+		try (Session s = sf.getCurrentSession()){
+			Transaction tx = s.beginTransaction();
+			items = s.createQuery("from Purchased").getResultList();
+			tx.commit();
+			s.close();
+		}
+		return items;
 	}
 
 	@Override
 	public void addPurchased(Purchased p) {
-		// TODO Auto-generated method stub
-		
+		try (Session s = sf.getCurrentSession()){
+			Transaction tx = s.beginTransaction();
+			s.persist(p);
+			tx.commit();
+			s.close();
+		}
 	}
 
 	@Override
 	public void updatePurchased(Purchased p) {
-		// TODO Auto-generated method stub
-		
+		try (Session s = sf.getCurrentSession()){
+			Transaction tx = s.beginTransaction();
+			s.update(p);
+			tx.commit();
+			s.close();
+		}
 	}
 
 	@Override
 	public void deletePurchased(Purchased p) {
-		// TODO Auto-generated method stub
-		
+		try (Session s = sf.getCurrentSession()){
+			Transaction tx = s.beginTransaction();
+			s.delete(p);
+			tx.commit();
+			s.close();
+		}
 	}
 
 }
