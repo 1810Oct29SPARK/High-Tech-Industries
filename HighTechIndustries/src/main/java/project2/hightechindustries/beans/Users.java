959d5d9f5c4b117d02dc504e37328790bd5c5d5c
Esteban Perez <esteban.perez7391@hotmail.com>
2019-01-05 14:12:21 -0500

    updated beans to be able to connect to db and grab data

--- HighTechIndustries/src/main/java/project2/hightechindustries/beans/Users.java
+++ HighTechIndustries/src/main/java/project2/hightechindustries/beans/Users.java
@@ -35,18 +35,19 @@ public class Users {
 		this.email = email;
 		this.phone = phone;
 		this.employeeStatus = employeeStatus;
-		this.helpedBy = helpedBy;
 		this.picture = picture;
 		this.username = username;
 		this.passHash = passHash;
 		this.salt = salt;
 	}
 	
-	public Users() {};
+	public Users() {
+		
+	}
 	
 	@Id
-	@Column(name="USER_ID")
-	private int id;
+	@Column(name="MEMBER_ID")
+	private Integer id;
 	@Column(name="FIRST_NAME")
 	private String firstName;
 	@Column(name="LAST_NAME")
@@ -58,7 +59,7 @@ public class Users {
 	@Column(name="EMPLOYEE_STATUS")
 	private String employeeStatus;
 	@Column(name="HELPED_BY")
-	private int helpedBy;
+	private Integer helpedBy;
 	@Column(name="IMAGE")
 	private Blob picture;
 	@Column(name="MEMBER_NAME")
@@ -139,85 +140,6 @@ public class Users {
 		return "Users [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", phone=" + phone + ", employeeStatus=" + employeeStatus + ", helpedBy=" + helpedBy + ", picture="
 				+ picture + ", username=" + username + ", passHash=" + passHash + ", salt=" + salt + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((employeeStatus == null) ? 0 : employeeStatus.hashCode());
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result + helpedBy;
-		result = prime * result + id;
-		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-		result = prime * result + ((passHash == null) ? 0 : passHash.hashCode());
-		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
-		result = prime * result + ((picture == null) ? 0 : picture.hashCode());
-		result = prime * result + ((salt == null) ? 0 : salt.hashCode());
-		result = prime * result + ((username == null) ? 0 : username.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Users other = (Users) obj;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (employeeStatus == null) {
-			if (other.employeeStatus != null)
-				return false;
-		} else if (!employeeStatus.equals(other.employeeStatus))
-			return false;
-		if (firstName == null) {
-			if (other.firstName != null)
-				return false;
-		} else if (!firstName.equals(other.firstName))
-			return false;
-		if (helpedBy != other.helpedBy)
-			return false;
-		if (id != other.id)
-			return false;
-		if (lastName == null) {
-			if (other.lastName != null)
-				return false;
-		} else if (!lastName.equals(other.lastName))
-			return false;
-		if (passHash == null) {
-			if (other.passHash != null)
-				return false;
-		} else if (!passHash.equals(other.passHash))
-			return false;
-		if (phone == null) {
-			if (other.phone != null)
-				return false;
-		} else if (!phone.equals(other.phone))
-			return false;
-		if (picture == null) {
-			if (other.picture != null)
-				return false;
-		} else if (!picture.equals(other.picture))
-			return false;
-		if (salt == null) {
-			if (other.salt != null)
-				return false;
-		} else if (!salt.equals(other.salt))
-			return false;
-		if (username == null) {
-			if (other.username != null)
-				return false;
-		} else if (!username.equals(other.username))
-			return false;
-		return true;
 	}
 
 }
