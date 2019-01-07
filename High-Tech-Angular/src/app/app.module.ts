5dd1a667941f3f96c8b1794fb8132214904392c7
jerbjer <jerbjer@gmail.com>
2019-01-07 11:38:05 -0500

    routing and page components

--- High-Tech-Angular/src/app/app.module.ts
+++ High-Tech-Angular/src/app/app.module.ts
@@ -1,20 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { NewsComponent } from './news/news.component';
+import { NewsPageComponent } from './news-page/news-page.component';
+import { EmployeePageComponent} from './employee-page/employee-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { StorePageComponent } from './store-page/store-page.component';
+
+const appRoutes: Routes = [
+  {path: '', 
+  redirectTo: 'news',
+  pathMatch: 'full'},
+  {path: 'news',
+  component: NewsPageComponent},
+  {path: 'profile',
+  component: ProfilePageComponent},
+  {path: 'store',
+  component: StorePageComponent},
+  {path: 'employee',
+  component: EmployeePageComponent},
+  { path: '**',
+  redirectTo: 'news',
+  pathMatch: 'full'
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FooterComponent,
-    NewsComponent
+    NewsComponent,
+    NewsPageComponent,
+    EmployeePageComponent,
+    ProfilePageComponent,
+    StorePageComponent
   ],
   imports: [
     BrowserModule, 
+    RouterModule.forRoot(
+      appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
