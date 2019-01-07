55d50f1d5c8aaccc38d38561048ebbdd95365b0e
Stewart Gardner <StewartBenGardner@gmail.com>
2019-01-07 14:27:48 -0500

    more comments

--- HighTechIndustries/src/main/java/project2/hightechindustries/main/Driver.java
+++ HighTechIndustries/src/main/java/project2/hightechindustries/main/Driver.java
@@ -29,14 +29,26 @@ public class Driver {
 		
 		CartDAO c = new CartDAOImpl();
 		PurchasedDAO p = new PurchasedDAOImpl();
-//		Users ou = u.getUserById(10);
-//		u.deleteUser(ou);
-//		c.deleteItem(new Cart(1, 12, 54));
-//		UserDAO u = new UserDAOImpl();
-//		StoreDAO s = new StoreDAOImpl();
-//		RecentlyViewedDAO rv = new RecentlyViewedDAOImpl();
-//		CartDAO c = new CartDAOImpl();
-<<<<<<< HEAD
+		p.addPurchased(new Purchased(1,3,13));
+		p.addPurchased(new Purchased(1,6,23));
+		List<Purchased> purchasedList = p.getPurchasedItemsByProductId(3);
+		for (Purchased pc : purchasedList) {
+			System.out.println(pc);
+		}
+//		System.out.println("owned by mario");
+//		List<Purchased> ownedList = p.getPurchasedItemsByMemberId(1);
+//		for (Purchased pc : ownedList) {
+//			System.out.println(pc);
+//		}
+//		System.out.println("owned by mario");
+//		PurchasedDAO pu = new PurchasedDAOImpl();
+//		p.addPurchased(new Purchased(1,3,13));
+//		p.addPurchased(new Purchased(1,6,23));
+//		p.deletePurchased(new Purchased(5));
+//		List<Purchased> purchasedList = p.getPurchasedItems(3);
+//		for (Purchased pc : purchasedList) {
+//			System.out.println(pc);
+//		}
 //		p.addPurchased(new Purchased(1,3,13));
 //		p.addPurchased(new Purchased(1,6,23));
 //		List<Purchased> purchasedList = p.getPurchasedItemsByProductId(3);
@@ -49,28 +61,6 @@ public class Driver {
 //			System.out.println(pc);
 //		}
 //		System.out.println("owned by mario");
-=======
-//		PurchasedDAO pu = new PurchasedDAOImpl();
-//		p.addPurchased(new Purchased(1,3,13));
-//		p.addPurchased(new Purchased(1,6,23));
-//		p.deletePurchased(new Purchased(5));
-//		List<Purchased> purchasedList = p.getPurchasedItems(3);
-//		for (Purchased pc : purchasedList) {
-//			System.out.println(pc);
-//		}
-		p.addPurchased(new Purchased(1,3,13));
-		p.addPurchased(new Purchased(1,6,23));
-		List<Purchased> purchasedList = p.getPurchasedItemsByProductId(3);
-		for (Purchased pc : purchasedList) {
-			System.out.println(pc);
-		}
-		System.out.println("owned by mario");
-		List<Purchased> ownedList = p.getPurchasedItemsByMemberId(1);
-		for (Purchased pc : ownedList) {
-			System.out.println(pc);
-		}
-		System.out.println("owned by mario");
->>>>>>> 415d917650180dbccc044d1d1c19560b7271dba0
 //		c.getAllCartItemsById(2);
 //		c.deleteItem(new Cart(18));
 		
@@ -92,21 +82,14 @@ public class Driver {
 		
 //		rv.addRecentlyViewed(new RecentlyViewed(3,1,2,3,4,5));
 //		System.out.println(rv.getRecentlyViewedById(1));
-<<<<<<< HEAD
 //		rv.newRecentlyViewed(new RecentlyViewed(8));
 //		rv.addRecentlyViewed(new RecentlyViewed(8,6));
-		List<RecentlyViewed> rvList = rv.getAllRecentlyViewed();
-		for (RecentlyViewed rvs : rvList) {
-			System.out.println(rvs);
-		}
-=======
 //		List<RecentlyViewed> rvList = rv.getAllRecentlyViewed();
 //		for (RecentlyViewed rvs : rvList) {
 //			System.out.println(rv);
 //		}
 //		rv.newRecentlyViewed(new RecentlyViewed(5));
 //		rv.addRecentlyViewed(new RecentlyViewed(5,6));
->>>>>>> 415d917650180dbccc044d1d1c19560b7271dba0
 //		System.out.println(p.getPurchasedItemById(1));
 	}
 
