8fcf24f1dc2486a182dc5ee9a97865cba781e011
sean sulham <sean.sulham@hotmail.com>
2019-01-07 13:19:45 -0500

    Adding structure for junit testing

---
+++ HighTechIndustries/src/main/test/project2/hightechindustries/dao/test/RecentlyViewedDAOImplTest.java
@@ -0,0 +1,5 @@
+package project2.hightechindustries.dao.test;
+
+public class RecentlyViewedDAOImplTest {
+
+}
---
+++ HighTechIndustries/src/main/test/project2/hightechindustries/util/test/HibernateUtilTest.java
@@ -0,0 +1,5 @@
+package project2.hightechindustries.util.test;
+
+public class HibernateUtilTest {
+
+}
