e3b922b9fdb74836a85d98eb2dadf255d174db15
sean sulham <sean.sulham@hotmail.com>
2019-01-07 10:39:15 -0500

    Updating...stuff

--- HighTechIndustries/src/main/java/project2/hightechindustries/dao/CartDAO.java
+++ HighTechIndustries/src/main/java/project2/hightechindustries/dao/CartDAO.java
@@ -6,10 +6,9 @@ import project2.hightechindustries.beans.Cart;
 
 public interface CartDAO {
 	
-//	public Cart getCartById(int memberId, int productId);
 	public List<Cart> getAllCartItemsById(int memberId);
 	public void addCartItem(Cart c);
-//	public void updateCart(Cart c);
+	public void updateCart(Cart c);
 	public void deleteItem(Cart c);
 
 }
