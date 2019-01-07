0f978a0c8f6e0625c2873187f0b6918f137b73d8
sean sulham <sean.sulham@hotmail.com>
2019-01-07 12:04:05 -0500

    Added JavaDocs to UserDAO

--- HighTechIndustries/src/main/java/project2/hightechindustries/dao/UserDAO.java
+++ HighTechIndustries/src/main/java/project2/hightechindustries/dao/UserDAO.java
@@ -6,10 +6,39 @@ import project2.hightechindustries.beans.Users;
 
 public interface UserDAO {
 	
+	/**
+	 * @author (name=Esteban) (for method)
+	 * @author (name=Sean) (for JavaDoc)
+	 * 
+	 */
 	public Users getUserById(int id);
+	
+	/**
+	 * @author (name=Esteban) (for method)
+	 * @author (name=Sean) (for JavaDoc)
+	 * 
+	 */
 	public List<Users> getAllUsers();
+	
+	/**
+	 * @author (name=Esteban) (for method)
+	 * @author (name=Sean) (for JavaDoc)
+	 * This method is here so when a visitor becomes a member, he/she is added to the list of members.
+	 */
 	public void addUser(Users user);
+	
+	/**
+	 * @author (name=Esteban) (for method)
+	 * @author (name=Sean) (for JavaDoc)
+	 * This method is here so that members can update their information.
+	 */
 	public void updateUser(Users user);
+	
+	/**
+	 * @author (name=Esteban) (for method)
+	 * @author (name=Sean) (for JavaDoc)
+	 * This method is here so that members can be deleted from the system if they decide to cancel their membership.
+	 */
 	public void deleteUser(Users user);
 
 }
