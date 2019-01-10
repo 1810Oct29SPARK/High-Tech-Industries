c044d3fcf5c6762fc190ec4ad93123d109d0c1ac
sean sulham <sean.sulham@hotmail.com>
2019-01-10 18:07:56 -0500

    Updating

--- HighTechIndustries/src/main/java/project2/hightechindustries/controller/LoginController.java
+++ HighTechIndustries/src/main/java/project2/hightechindustries/controller/LoginController.java
@@ -1,7 +1,16 @@
 package project2.hightechindustries.controller;
 
-//@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+import project2.hightechindustries.service.LoginService;
+
+@Controller
 public class LoginController {
+	
+	@Autowired
+	public LoginController(LoginService loginService) {
+		super();
+	}
 
 }
--- HighTechIndustries/src/main/java/project2/hightechindustries/service/StoreService.java
+++
@@ -1,5 +0,0 @@
-package project2.hightechindustries.service;
-
-public interface StoreService {
-
-}
