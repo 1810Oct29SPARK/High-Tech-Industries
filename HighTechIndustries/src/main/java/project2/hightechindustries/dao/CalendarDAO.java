cb2a5948fa41ff85e01b41c811e451d2eb4b8f6b
Esteban Perez <esteban.perez7391@hotmail.com>
2019-01-11 18:27:40 -0500

    updated DalendarDAO and Impl

--- HighTechIndustries/src/main/java/project2/hightechindustries/dao/CalendarDAO.java
+++ HighTechIndustries/src/main/java/project2/hightechindustries/dao/CalendarDAO.java
@@ -6,7 +6,7 @@ import project2.hightechindustries.beans.Calendar;
 
 public interface CalendarDAO {
 	
-	public List<Calendar> getCalendarEventById(int memberId);
+	public List<Calendar> getCalendarEventById(int eventId);
 	public List<Calendar> getAllCalendarEvents(int memberId);
 	public void addCalendarEvent(Calendar cal);
 	public void updateCalendar(Calendar cal);
