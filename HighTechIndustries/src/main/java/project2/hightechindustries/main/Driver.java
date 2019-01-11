8113326c5ca9e2cab6827870e5d386b83b132518
Stewart Gardner <StewartBenGardner@gmail.com>
2019-01-10 19:20:38 -0500

    trying to fix merge issues between daos and dev

--- HighTechIndustries/src/main/java/project2/hightechindustries/main/Driver.java
+++ HighTechIndustries/src/main/java/project2/hightechindustries/main/Driver.java
@@ -8,27 +8,7 @@ import project2.hightechindustries.dao.RecentlyViewedDAOImpl;
 
 public class Driver {
 	
-	public static void main(String[] args) {
-		
-//		Cart c = new Cart(2, 2, 10);
-//		CartDAO cd = new CartDAOImpl();
-//		cd.addOrUpdateCartItem(c);
-		
-		RecentlyViewedDAO rv = new RecentlyViewedDAOImpl();
-		
-		rv.newRecentlyViewed(new RecentlyViewed(1));
-		
-//		System.out.println(rv.getRecentlyViewedById(8));
-//
-//		List<RecentlyViewed> recViewedL = rv.getAllRecentlyViewed(8);
-//		for (RecentlyViewed rvl : recViewedL) {
-//			System.out.println(rvl);
-//		}
-//		Purchased p = new Purchased(1, 6, 5);
-//		PurchasedDAO pd = new PurchasedDAOImpl();
-//		pd.addOrUpdatePurchasedItem(p);
-		
-		
+	public static void main(String[] args) {		
 		
 	}
 
