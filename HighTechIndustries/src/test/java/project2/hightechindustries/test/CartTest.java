2f369bb768dda5a24b8c3431650e4ca02f285bac
Esteban Perez <esteban.perez7391@hotmail.com>
2019-01-10 19:26:42 -0500

    adding new test file for cart, now moving from working from dao to tests branch

---
+++ HighTechIndustries/src/test/java/project2/hightechindustries/test/CartTest.java
@@ -0,0 +1,16 @@
+package project2.hightechindustries.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CartTest {
+
+	@Test
+	public void getAllCartItemsById() {
+		
+		
+		
+	}
+
+}
