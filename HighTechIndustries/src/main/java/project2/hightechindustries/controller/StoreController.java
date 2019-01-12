5bc193b50baf8ed9a71dd7e102dbda370c29e880
Esteban Perez <esteban.perez7391@hotmail.com>
2019-01-12 18:02:58 -0500

    able to connect again to server

--- HighTechIndustries/src/main/java/project2/hightechindustries/controller/StoreController.java
+++ HighTechIndustries/src/main/java/project2/hightechindustries/controller/StoreController.java
@@ -5,25 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import project2.hightechindustries.beans.RecentlyViewed;
 import project2.hightechindustries.dao.RecentlyViewedDAO;
 
-@Controller
+@RestController
 @RequestMapping(value="/store")
 public class StoreController {
 	
 	@Autowired
 	private RecentlyViewedDAO recentlyViewed;
 	
-	@RequestMapping(value="/{memberId}", method=RequestMethod.GET)
-	@ResponseBody
+	@GetMapping(value="/{memberId}")
 	public ResponseEntity<RecentlyViewed> getPurchasedItemsByMemberId(@PathVariable int memberId) {
 		List<RecentlyViewed> rv = recentlyViewed.getAllRecentlyViewed(memberId);
 		if (rv == null) {
